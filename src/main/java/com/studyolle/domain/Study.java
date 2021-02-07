636c7d4f93cc581d75fd653c9f86012795973d48
yujin song <uzini_@likelion.org>
2021-02-08 05:38:49 +0900

    스터디 - 테스트 작성 / 스터디 구성원 조회

--- src/main/java/com/studyolle/domain/Study.java
+++ src/main/java/com/studyolle/domain/Study.java
@@ -83,4 +83,9 @@ public class Study {
 	public boolean isManager(UserAccount userAccount) {
 		return this.managers.contains(userAccount.getAccount());
 	}
+
+	public void addMemeber(Account account) {
+		this.members.add(account);
+	}
+
 }
