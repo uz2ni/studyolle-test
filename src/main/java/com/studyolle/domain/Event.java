426440f03256d65038365055f7d71e0cf466942b
yujin song <uzini_@likelion.org>
2021-02-15 22:15:50 +0900

    모임 - 목록 조회

--- src/main/java/com/studyolle/domain/Event.java
+++ src/main/java/com/studyolle/domain/Event.java
@@ -9,6 +9,10 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@NamedEntityGraph(
+		name = "Event.withEnrollments",
+		attributeNodes = @NamedAttributeNode("enrollments")
+)
 @Entity
 @Getter
 @Setter
@@ -72,6 +76,10 @@ public class Event {
 		}
 
 		return false;
+	}
+
+	public int numberOfRemainSpots() {
+		return this.limitOfEnrollments - (int) this.enrollments.stream().filter(Enrollment::isAccepted).count();
 	}
 
 	private boolean isAlreadyEnrolled(UserAccount userAccount) {
